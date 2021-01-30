1753d4547805a543efff70c0c3295a3986b45653
lurdharry <azeezhammed57@gmail.com>
2021-01-30 14:57:15 +0100

    chore: set-up react-query

--- App.tsx
+++ App.tsx
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import MainNavigator from "./src/navigation";
+import LoadAssets from "./src/hooks/loadResource";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const client = new QueryClient();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const isLoadingComplete = LoadAssets();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <QueryClientProvider client={client}>
+        <NavigationContainer>
+          <MainNavigator />
+        </NavigationContainer>
+      </QueryClientProvider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
