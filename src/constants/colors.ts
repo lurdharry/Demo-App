15141af251893c1bc88d122827e0d2526c3807fe
lurdharry <azeezhammed57@gmail.com>
2021-01-30 14:51:21 +0100

    chore: add color collection

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+export default {
+  white: "#FFFFFF",
+  nobel: "#343866",
+  lynch: "#8F92B2",
+  border: "#E1E2F0",
+  main: "#ECEEF9",
+  purple: "#6D2077",
+  pablue: "#00AEC7",
+  grey: "#F5F6F7",
+  black: "#000000",
+};
