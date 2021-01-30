e4fb86ebb43eab44397957132ae24be1a95ce944
lurdharry <azeezhammed57@gmail.com>
2021-01-30 14:48:05 +0100

    feat: add custom fonts

---
+++ src/hooks/loadResource.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import * as Font from "expo-font";
+
+export default function LoadAssets() {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+  // Load any resources or data that we need prior to rendering the app
+  useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        // Load fonts
+        await Font.loadAsync({
+          "roboto-medium": require("../../assets/fonts/roboto-medium.ttf"),
+          "roboto-regular": require("../../assets/fonts/roboto-regular.ttf"),
+        });
+      } catch (e) {
+        // We might want to provide this error information to an error reporting service
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  return isLoadingComplete;
+}
