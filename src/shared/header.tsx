739070614e276a12af0d8b1d058c8430614df9ef
lurdharry <azeezhammed57@gmail.com>
2021-02-18 17:08:04 +0100

    chore: refactor files

--- src/shared/header.tsx
+++ src/shared/header.tsx
@@ -1,10 +1,9 @@
 import React, { ReactElement } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { HeaderStyles as styles } from "./styles";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import { BackArrow, Heart } from "../../assets/svgs";
+import { BackArrow } from "../../assets/svgs";
 import { MediumText, RegularText } from "./text";
-import colors from "../constants/colors";
 import { SharedElement } from "react-navigation-shared-element";
 
 interface props {
