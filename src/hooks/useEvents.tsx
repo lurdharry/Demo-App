b59fd7490737651485016528c505a0fcbd62afa8
lurdharry <azeezhammed57@gmail.com>
2022-02-09 21:58:09 +0100

    feat: update even status with react query

---
+++ src/hooks/useEvents.tsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useCallback, useMemo } from "react";
+import { useQuery, useQueryClient } from "react-query";
+import { EventList } from "../types/types.d";
+import { fetchEvents, updateEvent } from "../utils";
+
+export const useEvents = () => {
+  const queryClient = useQueryClient();
+
+  const { data, status, ...rest } = useQuery("events", fetchEvents);
+
+  const markEvent = useCallback((id: string) => {
+    queryClient.cancelQueries("events"); // cancel any ongoing query
+
+    const previousEvents = queryClient.getQueryData<EventList>("events");
+    const updatedEvent = previousEvents && updateEvent(previousEvents, id);
+
+    queryClient.setQueryData("events", updatedEvent);
+  }, []);
+
+  const checkedEventStatus = useCallback(
+    (id: string) => {
+      return data?.find(({ eventId }) => eventId === id)?.checked;
+    },
+    [data]
+  );
+
+  return { eventsData: data, status, markEvent, checkedEventStatus, ...rest };
+};
